27c2b7c3f818ac1391f095497aabd1e2b9296e95
Saeed <saeedmoeen@yahoo.com>
2019-03-14 15:45:19 +0330

    Initial commit

---
+++ app/src/main/java/com/example/newslider/StartSlider.java
@@ -0,0 +1,13 @@
+package com.example.newslider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartSlider extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_slider);
+    }
+}
